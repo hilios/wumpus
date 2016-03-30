2dff3f9d344fa2177c9e0bab85af5aaef449e3b5
Edson Hilios <edson.hilios@unear.net>
2016-03-30 18:00:44 -0300

    Add class to hold the environment common properties

---
+++ src/main/java/wumpus/Environment.java
@@ -0,0 +1,37 @@
+package wumpus;
+
+/**
+ * Represents the world environment.
+ */
+public class Environment {
+    public enum Items {
+        WUMPUS, PIT, HUNTER, GOLD
+    }
+
+    public enum Perceptions {
+        SCREAM, STENCH, BREEZE, GLITTER, BUMP
+    }
+
+    public enum Actions {
+        GO_FORWARD, TURN_LEFT, TURN_RIGHT, GRAB, SHOOT, NO_OP
+    }
+
+    public static String getIcon(Items item) {
+        switch (item) {
+            case WUMPUS: return "W";
+            case HUNTER: return "H";
+            case PIT: return "P";
+            case GOLD: return "$";
+        }
+        return " ";
+    }
+
+    public static String getIcon(Perceptions perception) {
+        switch (perception) {
+            case STENCH: return "~";
+            case BREEZE: return "≈";
+            case GLITTER: return "*";
+        }
+        return " ";
+    }
+}
